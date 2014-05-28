a44761e0bb193c9806322bd09573e91f26ac80ec
Rory McCune <raesene@gmail.com>
2014-05-28 20:28:37 +0100

    change module structure for ordinary dradis

--- lib/dradis/ip_list_upload/actions.rb
+++ lib/dradis/ip_list_upload/actions.rb
@@ -1,23 +1,19 @@
 module Dradis
   module IpListUpload
-    module Actions
-      def self.import(params={})
-        file_content = File.readlines(params[:file])
+    def self.import(params={})
+      file_content = File.readlines(params[:file])
+      @hosts_node = Node.new(:label => 'Hosts')
+      @hosts_node.save
 
-        @hosts_node = Node.new(:label => 'Hosts')
-        @hosts_node.save
+      hosts_array = Array.new
 
-        hosts_array = Array.new
+      file_content.each do |line|
+        line_hosts = line.split(',')
+        line_hosts.each {|host| hosts_array << host}
+      end
 
-        file_content.each do |line|
-          line_hosts = line.split(',')
-          line_hosts.each {|host| hosts_array << host}
-        end
-
-        hosts_array.each do |h|
-          h_node = Node.new(:label => h, :parent_id => @hosts_node)
-        end
-
+      hosts_array.each do |h|
+        h_node = Node.new(:label => h, :parent_id => @hosts_node)
       end
     end
   end
