5bec11a246d393163978c2baf2a7b16a96e7408b
Rory McCune <raesene@gmail.com>
2014-05-28 20:05:20 +0100

    added meta.rb

---
+++ lib/dradis/ip_list_upload/meta.rb
@@ -0,0 +1,14 @@
+module Dradis
+  module IpListUpload
+    module Meta
+      NAME = "IP List Upload"
+      module VERSION
+        MAJOR = 2
+        MINOR = 10
+        TINY = 0
+
+        STRING = [MAJOR, MINOR, TINY].join('.')
+      end
+    end
+  end
+end
